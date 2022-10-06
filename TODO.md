4e4eabe68ba2c3fcac24cce8171bf403b64dfb41
Paul Lavender-Jones <paul@lavender-jones.com>
2022-10-06 13:22:14 +0100

    Added git revision to backend

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
+Features
 - [x] Add Chakra UI
 - [x] Add idea of owner of room
 - [x] Add tracks to rooms
@@ -7,3 +8,7 @@
 - [ ] Add votes to tracks
 - [ ] Ping/pong to check if user is alive
 - [ ] Delete empty rooms
+
+Bugs
+- Sidepanel need a fixed/min width
+- Dark mode colours for lots of things need adjusting
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,10 @@ import cors from "cors";
 import { createSocketServer, rooms } from "./createServer";
 import http from "http";
 
+import childProcess from "child_process";
+
+const revision = childProcess.execSync("git rev-parse HEAD").toString().trim();
+
 const port = Number.parseInt(process.env.PORT!) || 8080;
 
 const app = express();
@@ -13,6 +17,8 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (_request, response) => {
+	response.write(`Revision ${revision.slice(0, 6)}\n`);
+	response.write(`\n`);
 	rooms.forEach((room) => {
 		response.write(`${room.name} - ${room.connections.size} connection(s)\n`);
 	});
