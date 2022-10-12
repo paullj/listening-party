5992e69cb9f08d63d4a3b51c86ed6e7c05922d15
Paul Lavender-Jones <paul@lavender-jones.com>
2022-10-12 14:19:56 +0100

    Added route based code splitting

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,15 +1,17 @@
+import { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { RoomProvider } from "./context/RoomContext";
 import { SocketProvider } from "./context/SocketContext";
 import UserInfo from "./components/home/UserInfo";
 
-import Home from "./routes/Home";
-import Room from "./routes/Room";
-import Error from "./routes/Error";
 import { FeedProvider } from "./context/FeedContext";
 import { MeshProvider } from "./context/MeshContext";
 import { QueueProvider } from "./context/QueueContext";
+
+const HomePage = lazy(() => import("./routes/Home"));
+const RoomPage = lazy(() => import("./routes/Room"));
+const ErrorPage = lazy(() => import("./routes/Error"));
 
 function App() {
 	return (
@@ -17,20 +19,20 @@ function App() {
 			<SocketProvider>
 				<RoomProvider>
 					<Routes>
-						<Route path="/" element={<Home />} />
+						<Route path="/" element={<HomePage />} />
 						<Route
 							path="/room/:id"
 							element={
 								<QueueProvider>
 									<FeedProvider>
 										<MeshProvider>
-											<Room />
+											<RoomPage />
 										</MeshProvider>
 									</FeedProvider>
 								</QueueProvider>
 							}
 						/>
-						<Route path="/error" element={<Error />} />
+						<Route path="/error" element={<ErrorPage />} />
 					</Routes>
 				</RoomProvider>
 			</SocketProvider>
